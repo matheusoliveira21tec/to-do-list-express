c04813be20b7cf5ab7d5fe8493b22f6ef5559b59
monegasco <mhoa.2000@gmail.com>
2022-07-27 14:42:24 -0300

    commit 4

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 require('./config/database');
 const checklistRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views', path.join(__dirname,'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checklist', checklistRouter);
 
 app.listen(3000,()=>{
