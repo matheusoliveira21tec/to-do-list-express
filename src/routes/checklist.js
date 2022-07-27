ced602d05ee414e423397f3f3a06101ef4c6cbf4
monegasco <mhoa.2000@gmail.com>
2022-07-27 00:39:58 -0300

    commit 3

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,21 +2,47 @@ const express = require('express');
 const Checklist = require('../models/checklist');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    console.log('passei por aqui');
-    res.send();
-})
-router.post('/', async(req,res)=>{
-    let{name}= req.body;
+router.get('/', async (req, res) => {
     try {
-        let checklist = await Checklist.create({name})
+        let checklists = await Checklist.find({});
+        res.status(200).send(checklists);
+    } catch (error) {
+        res.status(422).send(error);
+    }
+})
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+    try {
+        let checklist = await Checklist.create({ name })
         res.status(200).send(checklist);
     } catch (error) {
         res.status(422).send(error);
     }
 })
-router.get('/:id',(req,res)=>{
-    console.log(req.params.id);
-    res.send(`Id: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).send(error);
+    }
+})
+router.put('/:id', async (req, res) => {
+    let { name } = req.body
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
+        res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).send(error);
+    }
+})
+router.delete('/:id', async (req, res) => {
+    let { name } = req.body
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).send(error);
+    }
 })
 module.exports = router;
